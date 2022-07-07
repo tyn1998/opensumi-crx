3719cc78eace3570b516af2fc891f575d52c7156
Lam Tang <tangyenan@gmail.com>
2022-07-07 10:11:10 +0800

    useEffect not used, remvoe it

--- src/pages/Panel/Panel.jsx
+++ src/pages/Panel/Panel.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './Panel.css';
 import { startCapturing, stopCapturing, getMessages } from '../../capturer';
 
