f9df3465748d9dc2e26d54e27561f2971bacf46f
Lam Tang <tangyenan@gmail.com>
2022-07-07 10:11:34 +0800

    capture messages from @opensumi/ide-connection and show them in a demo (#10)
    * capture messages from @opensumi/ide-connection and show them in a demo
    
    * useEffect not used, remvoe it

--- src/pages/Devtools/index.js
+++ src/pages/Devtools/index.js
@@ -1,18 +1,3 @@
-chrome.devtools.inspectedWindow.eval(`console.log('你好牛你好牛')`);
-
-chrome.devtools.inspectedWindow
-  .eval(`window.__OPENSUMI_DEVTOOL_EVENT_SOURCE_TOKEN__ = {
-    traffic: {
-      send: (msg) => {
-          console.log('[send] ', msg)
-      },
-      receive: (msg) => {
-          console.log('[receive] ', msg)
-      },
-    },
-  };
-`);
-
 chrome.devtools.panels.create(
   'OpenSumi DevTools',
   'logo.png',
